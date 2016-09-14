54bb182e68a8c839619e88b3d7d90953c8addbf0
Gabriel Imre <gabrielimre@work.local>
2016-09-14 08:57:09 +0300

    updated

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 'use strict'
 
 const JiraApi = require( 'jira' ).JiraApi
-const jira = new JiraApi( 'https', 'devnet.eggs.de', 80, 'gimre', 'aN0mW9zg#','2.0.alpha1', null, false, null, 'jira' )
+const jira = new JiraApi( 'https', 'devnet.eggs.de', 80, 'user', 'pass','2.0.alpha1', null, false, null, 'jira' )
 
 jira.listProjects( projects => console.log( projects ) )
